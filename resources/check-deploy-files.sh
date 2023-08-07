955e5f6a06734e13337dafde4b1c8d0c9a466565
lucas.scrini@hotmail.com <lucasscrini@Lucass-MacBook-Air.local>
2023-08-07 09:33:31 -0300

    Update shs and gitignore

--- resources/check-deploy-files.sh
+++ resources/check-deploy-files.sh
@@ -7,15 +7,16 @@ wildfly_deployment_dir=/app/wildfly/standalone/deployments
 
 check_wars() {
     # Verifies if has some war at deployments directory
-    if [ -f "$deployments_dir/*.war" ]; then
+    if [ -f $deployments_dir/*.war ]; then
         # Get list of wars.
         wars=$(ls $deployments_dir/*.war)
         # For each war, check if is the same of present in WildFly deployment directory
         for war in $wars; do
             # Get war name
             war_name=$(basename $war)
+
             # Verifies if the artifacts are different from the ones in the WildFly directory.
-            if ! cmp -s "$deployments_dir/$war_name" "$wildfly_deployment_dir/$war_name"; then
+            if ! cmp -s "$deployments_dir/$war_name" "$wildfly_deployment_dir/$war_name" ; then
                 # Moves the artifacts to the WildFly directory.
                 cp "$deployments_dir/$war_name" "$wildfly_deployment_dir/$war_name"
                 echo "$war_name moved to WildFly directory."
